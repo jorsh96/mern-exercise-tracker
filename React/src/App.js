d6b915add1c0dc71836b13a1e12bb1c9555d9935
jorsh96 <excellence.olatunbosun@decagon.dev>
2021-11-22 19:37:04 +0100

    my website
    a fully responsive website, fully exploring the full potentials of reusable components

---
+++ React/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
